f214a889187ee01baade53eda097a53aa6a72ff5
Anthony Cailleau <135410178+anthonycailleau@users.noreply.github.com>
2024-05-31 00:55:10 +0200

    déploiement

--- public/index.php
+++ public/index.php
@@ -1,51 +1,38 @@
-<?php 
-include dirname(__DIR__).'/books.php';
-date_default_timezone_set("Europe/Paris");
-?>
+<?php include dirname(__DIR__).'/includes/books.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kindle</title>
-    <link rel="stylesheet" href="/style.css">
-    <script src="/script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
 </head>
-
 <body>
     <div class="container">
         <div class="kindle">
             <div class="screen">
                 <div class="screen-content">
                     <div class="header">
-                        <a href="/index.php">⌂ Home</a>
+                        <a href="index.php">⌂ Home</a>
                         <div class="date">
-                        <?= date("H") . "h" . date("i") ?>
-                            <!-- 10h25 -->
+                            10h25
                         </div>
                     </div>
                     <h2> Ma liste de livres </h2>
                     <div class="grid-content" id="bookContainer">
-                        
-                        <!-- Mettre le contenu à afficher dans cette div -->
-                        <!-- je commence par boucler sur le tableau contenu dans books.php en itérant sur chaque livre contenu 
-                        dans le tableau $books -->
                         <?php foreach ($books as $key => $book) : ?>
-                            <!-- J'intègre les liens vers les pages dédiées en récupérant l'identifiant du livre comment paramètre GET-->
-                            <a href="/livre.php?title=<?php echo $key; ?>">
+                            <a href="livre.php?id=<?php echo $key; ?>">
                                 <div class="book">
-                                    <!-- Gestion de l'affichage de l'image du livre, le nom de fichier est basé sur la clé du tableau -->
-                                    <img src="/images/<?php echo $key; ?>.jpg" alt="<?php echo $book['titre']; ?>">
+                                    <img src="images/<?php echo $key; ?>.jpg" alt="<?php echo $book['titre']; ?>">
                                 </div>
                             </a>
                         <?php endforeach ?>
                     </div>
-
                     <div class="list-content hidden" id="listContainer">
                         <?php foreach ($books as $key => $book) : ?>
-                            <a href="/livre.php?title=<?php echo $key; ?>">
+                            <a href="livre.php?id=<?php echo $key; ?>">
                                 <div class="books-detail">
                                     <h4><?php echo $book['titre'] ?></h4>
                                     <p><?php echo $book['auteur'] ?></p>
@@ -63,5 +50,4 @@ date_default_timezone_set("Europe/Paris");
         </div>
     </div>
 </body>
-
 </html>
--- public/livre.php
+++ public/livre.php
@@ -1,35 +1,29 @@
-<?php date_default_timezone_set("Europe/Paris"); ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kindle</title>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
     <div class="container">
         <div class="kindle">
             <div class="screen">
                 <div class="screen-content">
                     <div class="header">
-                        <a href="/index.php">⌂ Home</a>
+                        <a href="index.php">⌂ Home</a>
                         <div class="date">
-                            <!-- 10h25 -->
-                            <?= date("H") . "h" . date("i") ?>
+                            10h25
                         </div>
                     </div>
                     <div class="book-content">
                         <?php
-                        // Je récupère l'id du livre depuis le paramètre GET 
-                        if (isset($_GET['title'])) {
-                            $key = $_GET['title'];
-                            // J'inclus le fichier correspondant au livre depuis le dossier livres/
-                            include dirname(__DIR__)."/livres/$key.php";
+                        if (isset($_GET['id'])) {
+                            $id = $_GET['id'];
+                            include "../livres/$id.php";
                         } else {
-                            echo "Aucun livre selectionné";
+                            echo "Aucun livre sélectionné";
                         }
                         ?>
                     </div>
