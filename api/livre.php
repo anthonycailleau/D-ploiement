3738dcb98dcd8012b3cceb4a6cc14ded117cd4d0
Anthony Cailleau <135410178+anthonycailleau@users.noreply.github.com>
2024-05-31 02:35:31 +0200

    déploiement

--- api/livre.php
+++ api/livre.php
@@ -12,7 +12,7 @@
             <div class="screen">
                 <div class="screen-content">
                     <div class="header">
-                        <a href="/api/index.php">⌂ Home</a>
+                        <a href="/">⌂ Home</a>
                         <div class="date">
                             <?= date("H") . "h" . date("i") ?>
                         </div>
@@ -22,8 +22,13 @@
                         // Récupère l'id du livre depuis le paramètre GET
                         if (isset($_GET['id'])) {
                             $id = $_GET['id'];
-                            // Inclut le fichier correspondant au livre depuis le dossier livres/
-                            include "../livres/$id.php";
+                            // Vérifie si le fichier du livre existe avant de l'inclure
+                            $livre_path = __DIR__ . "/../livres/$id.php";
+                            if (file_exists($livre_path)) {
+                                include $livre_path;
+                            } else {
+                                echo "Livre non trouvé.";
+                            }
                         } else {
                             echo "Aucun livre sélectionné";
                         }
